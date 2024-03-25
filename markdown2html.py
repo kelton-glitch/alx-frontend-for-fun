09638c85d1fc60336dec6165e882a58d21bdac22
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-25 13:51:50 +0000

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -2,6 +2,7 @@
 import sys
 import os.path
 
+
 def convert_markdown_to_html(markdown_file, output_file):
     if not os.path.exists(markdown_file):
         print(f"Missing {markdown_file}", file=sys.stderr)
@@ -10,11 +11,14 @@ def convert_markdown_to_html(markdown_file, output_file):
     # Your code to convert Markdown to HTML goes here
     # Replace this comment with your actual implementation
 
-    print(f"Markdown file '{markdown_file}' successfully converted to HTML and saved to '{output_file}'")
+    print(f"Markdown file '{markdown_file}' successfully
+          converted to HTML and saved to '{output_file}'")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        print("Usage: ./markdown2html.py <markdown_file> <output_file>",
+              file=sys.stderr)
         sys.exit(1)
 
     markdown_file = sys.argv[1]
